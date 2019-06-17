4195dfe492144ce072585d867562fed790351306
Alvaro Saurin <alvaro.saurin@gmail.com>
2019-06-17 11:45:00 +0200

    Improved installation script

--- internal/assets/generated_kubeadm_setup.go
+++ internal/assets/generated_kubeadm_setup.go
@@ -8,27 +8,30 @@ const KubeadmSetupScriptCode = `#!/bin/sh
 # kubeadm setup script
 ##########################################################################################
 
-LSB_RELEASE=/usr/bin/lsb_release
+LSB_RELEASE="/usr/bin/lsb_release"
 
 # the executable that packages will install, and the packages per distro
-KUBEADM_EXE=/usr/bin/kubeadm
+KUBEADM_EXE="/usr/bin/kubeadm"
 
-KUBEADM_PKG_SUSE="kubernetes-kubeadm"
-KUBEADM_PKG_SUSE_REPO="https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Leap_15.1/"
-KUBEADM_PKG_SUSE_REPOFILE="/etc/zypp/repos.d/kubernetes.repo"
-KUBEADM_PKG_SUSE_PACKAGES="$KUBEADM_PKG_SUSE kubernetes-kubelet kubernetes-client"
+PKG_SUSE="kubernetes-kubeadm"
+PKG_SUSE_REPO="https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Leap_15.1/"
+PKG_SUSE_REPOFILE="/etc/zypp/repos.d/kubernetes.repo"
+PKG_SUSE_PACKAGES="$PKG_SUSE kubernetes-kubelet kubernetes-client"
 
-KUBEADM_PKG_APT="kubeadm"
-KUBEADM_PKG_APT_REPO="http://apt.kubernetes.io/"
-KUBEADM_PKG_APT_GPG="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
-KUBEADM_PKG_APT_PACKAGES="$KUBEADM_PKG_APT kubelet kubectl docker.io kubernetes-cni"
-KUBEADM_PKG_APT_PACKAGES_PRE="apt-transport-https ebtables ethtool"
-KUBEADM_PKG_APT_SRCLST="/etc/apt/sources.list.d/kubernetes.list"
+PKG_APT="kubeadm"
+PKG_APT_REPO="http://apt.kubernetes.io/"
+PKG_APT_GPG="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+PKG_APT_PACKAGES="$PKG_APT kubelet kubectl docker.io kubernetes-cni"
+PKG_APT_PACKAGES_PRE="apt-transport-https ebtables ethtool"
+PKG_APT_SRCLST="/etc/apt/sources.list.d/kubernetes.list"
 
-KUBEADM_PKG_YUM="kubeadm"
-KUBEADM_PKG_YUM_REPOFILE="/etc/yum.repos.d/kubernetes.repo"
-KUBEADM_PKG_YUM_PACKAGES="$KUBEADM_PKG_YUM kubelet kubernetes-cni docker kubectl"
-KUBEADM_PKG_YUM_DEF_RELEASE=7
+PKG_YUM="kubeadm"
+PKG_YUM_REPOFILE="/etc/yum.repos.d/kubernetes.repo"
+PKG_YUM_PACKAGES="$PKG_YUM kubelet kubernetes-cni docker kubectl"
+PKG_YUM_DEF_RELEASE=7
+
+ZYPPER_AR_ARGS="--non-interactive"
+ZYPPER_IN_ARGS="-y --no-recommends --auto-agree-with-licenses"
 
 # we will try to discover the DIST and RELEASE
 ID=
@@ -37,7 +40,8 @@ RELEASE=
 
 ##########################################################################################
 
-log()    { echo "[kubeadm setup script] $@" ;     }
+log()    { echo "[kubeadm setup script] $@" ; }
+warn()   { log "WARNING!!!!: $@" ; }
 abort()  { log "FATAL!!!!: $@" ; exit 1 ; }
 
 restart_services() {
@@ -51,27 +55,20 @@ restart_services() {
 # installation for SUSE variants: OpenSUSE/SLE/CaaSP...
 install_zypper() {
     source /etc/os-release
+    local repo_name=$(basename $PKG_SUSE_REPOFILE .repo)
 
-    if [ ! -f $KUBEADM_PKG_SUSE_REPOFILE ] ; then
-        local name=$(echo $NAME | tr " " "_")
-        local ver=$VERSION
-
-        cat <<EOF > $KUBEADM_PKG_SUSE_REPOFILE
-[kubernetes]
-name=Kubernetes
-baseurl=https://download.opensuse.org/repositories/devel:/kubic/$name_$ver/
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-EOF
+    if [ ! -f $PKG_SUSE_REPOFILE ] ; then
+        log "adding repo from $PKG_SUSE_REPO..."
+        zypper $ZYPPER_AR_ARGS --quiet addrepo --refresh $PKG_SUSE_REPO $repo_name
     else
         log "repository already found: skipping installation of the repo"
     fi
-    zypper refresh
+    log "refreshing packages..."
+    zypper $ZYPPER_AR_ARGS --gpg-auto-import-keys refresh $repo_name
 
     log "checking we have everything we need..."
-    zypper $zypper_args in -y $KUBEADM_PKG_SUSE_PACKAGES || \
-        (abort "could not finish the installation of kubeadm" && rm -f $KUBEADM_PKG_SUSE_REPOFILE)
+    zypper in $ZYPPER_IN_ARGS $PKG_SUSE_PACKAGES || \
+        (abort "could not finish the installation of kubeadm" && rm -f $PKG_SUSE_REPOFILE)
     log "... everything installed"
     restart_services
 }
@@ -79,9 +76,9 @@ EOF
 # installation for RedHat variants: RedHat/CentOS...
 install_yum() {
     log "Installing for RedHat..."
-    if [ ! -f $KUBEADM_PKG_YUM_REPOFILE ] ; then
-        [ -n "$RELEASE" ] || RELEASE=$KUBEADM_PKG_YUM_DEF_RELEASE
-        cat <<EOF > $KUBEADM_PKG_YUM_REPOFILE
+    if [ ! -f $PKG_YUM_REPOFILE ] ; then
+        [ -n "$RELEASE" ] || RELEASE=$PKG_YUM_DEF_RELEASE
+        cat <<EOF > $PKG_YUM_REPOFILE
 [kubernetes]
 name=Kubernetes
 baseurl=http://yum.kubernetes.io/repos/kubernetes-el$RELEASE-x86_64
@@ -106,8 +103,8 @@ EOF
     fi
 
     log "checking we have everything we need..."
-    yum install -y $KUBEADM_PKG_YUM_PACKAGES || \
-        (abort "could not finish the installation of kubeadm" && rm -f $KUBEADM_PKG_YUM_REPOFILE)
+    yum install -y $PKG_YUM_PACKAGES || \
+        (abort "could not finish the installation of kubeadm" && rm -f $PKG_YUM_REPOFILE)
     log "... everything installed"
 
     # we must use the "cgroupfs"
@@ -120,19 +117,19 @@ EOF
 # installation for Debian variants: debian/Ubuntu...
 install_apt() {
     log "installing for Ubuntu|Debian..."
-    if [ ! -f $KUBEADM_PKG_APT_SRCLST ] ; then
-        apt-get update && apt-get install -y $KUBEADM_PKG_APT_PACKAGES_PRE || \
-            (abort "could not finish the installation of the requirements" && rm -f $KUBEADM_PKG_APT_SRCLST)
-        curl -s "$KUBEADM_PKG_APT_GPG" | apt-key add -
-        echo "deb $KUBEADM_PKG_APT_REPO kubernetes-xenial main" >> $KUBEADM_PKG_APT_SRCLST
+    if [ ! -f $PKG_APT_SRCLST ] ; then
+        apt-get update && apt-get install -y $PKG_APT_PACKAGES_PRE || \
+            (abort "could not finish the installation of the requirements" && rm -f $PKG_APT_SRCLST)
+        curl -s "$PKG_APT_GPG" | apt-key add -
+        echo "deb $PKG_APT_REPO kubernetes-xenial main" >> $PKG_APT_SRCLST
     else
         log "repository already found: skipping installation of the repo"
     fi
     apt-get update
 
     log "checking we have everything we need..."
-    [ -x $KUBEADM_EXE ] || apt-get install -y $KUBEADM_PKG_APT_PACKAGES || \
-        (abort "could not finish the installation of kubeadm" && rm -f $KUBEADM_PKG_APT_SRCLST)
+    [ -x $KUBEADM_EXE ] || apt-get install -y $PKG_APT_PACKAGES || \
+        (abort "could not finish the installation of kubeadm" && rm -f $PKG_APT_SRCLST)
     log "... everything installed"
     restart_services
 }
@@ -169,21 +166,36 @@ if [ -x $LSB_RELEASE ] ; then
 
     *)
         log "could not get the release/distribution from $LSB_RELEASE"
-        DIST=
         ;;
     esac
-else
-    if [ -f /etc/debian_version ] ; then
+elif [ /etc/os-release ] ; then
+    source /etc/os-release
+    case $NAME in
+    RedHatEnterpriseServer|CentOS|Fedora)
+        install_yum
+        ;;
+    Ubuntu|Debian)
         install_apt
-    elif [ -f /etc/fedora-release ] ; then
-        install_yum
-    elif [ -f /etc/redhat-release ] ; then
-        install_yum
-    elif [ -f /etc/SuSE-release ] ; then
+        ;;
+    *SUSE*)
         install_zypper
-    elif [ -f /etc/centos-release ] ; then
-        install_yum
-    fi
+        ;;
+    *)
+        warn "could not get the release/distribution from /etc/os-release"
+        ;;
+    esac
+elif [ -f /etc/debian_version ] ; then
+        install_apt
+elif [ -f /etc/fedora-release ] ; then
+    install_yum
+elif [ -f /etc/redhat-release ] ; then
+    install_yum
+elif [ -f /etc/SuSE-release ] ; then
+    install_zypper
+elif [ -f /etc/centos-release ] ; then
+    install_yum
+else
+    warn "No distro detected"
 fi
 
 [ -x $KUBEADM_EXE ] || abort "no kubeadm executable available at $KUBEADM_EXE"
