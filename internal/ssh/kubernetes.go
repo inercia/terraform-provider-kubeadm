9809f1043956521a82f1823b0324d33275732e56
Alvaro Saurin <alvaro.saurin@gmail.com>
2019-06-21 17:06:46 +0200

    Refactor out some function for running a local kubectl
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- internal/ssh/kubernetes.go
+++ internal/ssh/kubernetes.go
@@ -6,23 +6,25 @@ import (
 	"os/exec"
 )
 
-// DoLocalKubectlApply applies some manifests with a local kubectl
-func DoLocalKubectlApply(kubeconfig string, manifests []string) ApplyFunc {
+// DoLocalKubectl runs a local kubectl command
+func DoLocalKubectl(kubeconfig string, args ...string) ApplyFunc {
 	kubectl, err := exec.LookPath("kubectl")
 	if err != nil {
 		log.Fatal("kubectl not available")
 	}
 
+	f := append([]string{fmt.Sprintf("--kubeconfig=%s", kubeconfig)}, args...)
+	return DoLocalExec(kubectl, f...)
+}
+
+// DoLocalKubectlApply applies some manifests with a local kubectl
+func DoLocalKubectlApply(kubeconfig string, manifests []string) ApplyFunc {
 	loaders := []Applyer{}
 	for _, manifest := range manifests {
 		if len(manifest) == 0 {
 			continue
 		}
-
-		loaders = append(loaders,
-			DoLocalExec(kubectl,
-				fmt.Sprintf("--kubeconfig=%s", kubeconfig),
-				"apply", "-f", manifest))
+		loaders = append(loaders, DoLocalKubectl(kubeconfig, "apply", "-f", manifest))
 	}
 	return ApplyComposed(loaders...)
 }
