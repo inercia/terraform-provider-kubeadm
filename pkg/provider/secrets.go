a60c0f829e8523da8eda4042d14cbd64e0210bfb
Alvaro Saurin <alvaro.saurin@gmail.com>
2019-06-21 17:06:08 +0200

    Some not-yet-functional code for draining nodes.
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

---
+++ pkg/provider/secrets.go
@@ -0,0 +1,13 @@
+package provider
+
+var Secrets = []string{
+	"pki/ca.crt",
+	"pki/ca.key",
+	"pki/sa.key",
+	"pki/sa.pub",
+	"pki/front-proxy-ca.crt",
+	"pki/front-proxy-ca.key",
+	"pki/etcd/ca.crt",
+	"pki/etcd/ca.key",
+	"admin.conf",
+}
