cf22b02fe0a012a0aabdc7c4fa2cdf01dac5b9b6
Alvaro Saurin <alvaro.saurin@gmail.com>
2019-07-03 12:00:10 +0200

    Improved tests
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- tests/e2e/01-cluster-creation.sh
+++ tests/e2e/01-cluster-creation.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+###########################################################################################
+# variables
+###########################################################################################
+
 [ -f common.bash  ] && source common.bash
 [ -f dynamic.bash ] && source dynamic.bash
 [ -f local.bash   ] && source local.bash
 
 TF_ARGS=""
+NUM_MASTERS=2
+NUM_WORKERS=2
 
 cd $E2E_ENV
-
 [ -f "ci.tfvars" ] && TF_ARGS="$TF_ARGS -var-file=ci.tfvars"
+
+###########################################################################################
+# cluster creation
+###########################################################################################
 
 echo ">>> Terraform info:"
 terraform --version
@@ -30,10 +39,40 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-echo ">>> Getting cluster info with kubectl..."
-kubectl --kubeconfig=kubeconfig.local get nodes --show-labels
+###########################################################################################
+# checks
+###########################################################################################
+
+echo ">>> Checking we can get cluster info with kubectl..."
+kubectl --kubeconfig=kubeconfig.local get nodes
+if [ $? -ne 0 ] ; then
+    echo ">>> FAILED: could not get the nodes with kubectl"
+    exit 1
+fi
+
+OUT=$(kubectl --kubeconfig=kubeconfig.local get nodes --show-labels)
 if [ $? -ne 0 ] ; then
     echo ">>> FAILED: could not get the number of nodes with kubectl"
     exit 1
 fi
 
+EXP_NUM_NODES=$((NUM_MASTERS + NUM_WORKERS))
+echo ">>> Checking we have $EXP_NUM_NODES nodes..."
+CURR_NUM_NODES=$(echo "$OUT" | grep -c "kubernetes.io/hostname")
+if [ $CURR_NUM_NODES -ne $EXP_NUM_NODES ] ; then
+    echo ">>> FAILED: current number of nodes, $CURR_NUM_NODES, do not match $EXP_NUM_NODES"
+    exit 1
+fi
+
+EXP_NUM_MASTERS=$NUM_MASTERS
+CURR_NUM_MASTERS=$(echo "$OUT" | grep -c "node-role.kubernetes.io/master")
+if [ $CURR_NUM_MASTERS -ne $EXP_NUM_MASTERS ] ; then
+    echo ">>> FAILED: current number of masters, $CURR_NUM_MASTERS, do not match $EXP_NUM_MASTERS"
+    exit 1
+fi
+
+###########################################################################################
+# teardown
+###########################################################################################
+
+# TODO: do not destroy the cluster until we fix the DnD problems
